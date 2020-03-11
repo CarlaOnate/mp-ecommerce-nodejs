d1b6a1feeb15a07e965c7a0e18f1cad89d50d4e5
CarlaOnate <carlaonategardella@gmail.com>
2020-03-11 12:55:18 -0600

    intento implementacion mercado pago pt.1

--- app.js
+++ app.js
@@ -1,10 +1,39 @@
 var express = require('express');
 var exphbs  = require('express-handlebars');
- 
+// SDK de Mercado Pago
+const mercadopago = require ('mercadopago');
+
 var app = express();
- 
+
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
+
+// Agrega credenciales
+mercadopago.configure({
+  access_token: process.env.ACCESS_TOKEN
+});
+
+// Crea un objeto de preferencia
+let preference = {
+    items: [
+      {
+        title: 'Mi producto',
+        unit_price: 50,
+        quantity: 1,
+      }
+    ]
+  };
+
+mercadopago.preferences.create(preference)
+  .then(function(response){
+  // Este valor reemplazará el string "$$init_point$$" en tu HTML
+    global.init_point = response.body.init_point;
+  }).catch(function(error){
+    console.log(error);
+});
+
+
+
 
 app.get('/', function (req, res) {
     res.render('home');
@@ -13,6 +42,8 @@ app.get('/', function (req, res) {
 app.get('/detail', function (req, res) {
     res.render('detail', req.query);
 });
+
+
 
 app.use(express.static('assets'));
  
